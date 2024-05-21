b976743ae2ae34c40fd49934941a0ead7af458e4
Phati Sawant <61258100+Phati@users.noreply.github.com>
2024-05-21 21:43:07 +0530

    Update loadEnv.groovy

--- vars/loadEnv.groovy
+++ vars/loadEnv.groovy
@@ -1,3 +1,15 @@
 def call(){
-    sh "echo hello from shared library"
+    sh '''
+    echo loading .env file
+    def envFilePath = "${WORKSPACE}/.env"
+    if (fileExists(envFilePath)) {
+        def envContent = readFile(envFilePath).trim()
+        envContent.readLines().each { line ->
+        def (key, value) = line.split('=')
+        env."${key}" = value
+    }
+    } else {
+        echo "No .env file found"
+    }
+    '''
 }
